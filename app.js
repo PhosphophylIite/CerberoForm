92f751d5bc9f9fe74c0aa5967a61bb454d19211a
PhosphophylIite <114411654+PhosphophylIite@users.noreply.github.com>
2024-06-26 21:17:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 // Puerto utilizado por node.js y creación de la app con express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Conexión a la base de datos
 const connection = mysql.createConnection({
